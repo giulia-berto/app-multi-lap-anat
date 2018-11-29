2177123ee70853f0d37e0b432461d044ef16f061
Giulia Berto <gberto@hm012.karst.uits.iu.edu>
2018-11-29 12:33:15 -0500

    go back to NON PARALLEL slr

--- run.sh
+++ run.sh
@@ -89,32 +89,38 @@ fi
 
 
 echo "SLR registration"
-
-i=1
-id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
-tractogram_moving=tractograms_directory/$id_mov'_track.trk'
-python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk'
-echo "SLR registration of the first example done."
-
-for i in 2 3;
+for i in `seq 1 $num_ex`; 
 do
 	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
 	tractogram_moving=tractograms_directory/$id_mov'_track.trk'
-	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk' &
+	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk'
 done
-echo "Waiting for SLR registration of example 2 and 3 to finish"
-wait
-echo "SLR registration of example 2 and 3 done."
 
-for i in 4 5;
-do
-	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
-	tractogram_moving=tractograms_directory/$id_mov'_track.trk'
-	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk' &
-done
-echo "Waiting for SLR registration of example 4 and 5 to finish"
-wait
-echo "SLR registration of example 4 and 5 done."
+#i=1
+#id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
+#tractogram_moving=tractograms_directory/$id_mov'_track.trk'
+#python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk'
+#echo "SLR registration of the first example done."
+
+#for i in 2 3;
+#do
+#	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
+#	tractogram_moving=tractograms_directory/$id_mov'_track.trk'
+#	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk' &
+#done
+#echo "Waiting for SLR registration of example 2 and 3 to finish"
+#wait
+#echo "SLR registration of example 2 and 3 done."
+
+#for i in 4 5;
+#do
+#	id_mov=$(jq -r "._inputs[1+$i+$num_ex].meta.subject" config.json | tr -d "_")
+#	tractogram_moving=tractograms_directory/$id_mov'_track.trk'
+#	python tractograms_slr.py -moving $tractogram_moving -static $subjID'_track.trk' &
+#done
+#echo "Waiting for SLR registration of example 4 and 5 to finish"
+#wait
+#echo "SLR registration of example 4 and 5 done."
 
 
 echo "AFQ conversion to trk"
